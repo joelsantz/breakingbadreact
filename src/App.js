94bfedc6e5ce6da98bcc26f1866771d9fb86c00f
Joel Santana <joelsantzdev@gmail.com>
2019-07-26 00:44:23 -0500

    upload files project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function Frase({frase}) {
+  return (
+    <div className="frase">
+      <h1>{frase.quote}</h1>
+      <p>- {frase.author}</p>
+    </div>
+  )
+}
+
+function App() {
+
+  const [frase, obtenerFrase] = useState({});
+
+  const consultarAPI = async () => {
+      const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+
+      // console.log(resultado.data[0]);
+      // agregar el resultado de la API al state; (similar a this.setState)
+     obtenerFrase(resultado.data[0]);
   }
+
+  // consulta a una rest api
+  useEffect(
+    () => {
+      consultarAPI()
+    }, []
+  )
+  // console.log(frase);
+
+  return (
+    <div className="contenedor">
+      <Frase 
+        frase={frase}
+      />
+      <button
+        onClick={consultarAPI}
+      >Generate New</button>
+    </div>
+  )
 }
 
 export default App;
